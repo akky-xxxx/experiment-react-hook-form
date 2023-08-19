e37f765650f78b1cd1faff5083776c966b999834
akky-xxxx <114.s.aki@gmail.com>
2023-08-20 01:07:58 +0900

    feat: adjust schema

--- src/shared/schemas/passwordChangeSchema/index.ts
+++ src/shared/schemas/passwordChangeSchema/index.ts
@@ -1,12 +1,10 @@
-import { z } from "zod"
-
 import { passwordSchema } from "../passwordSchema"
 
 import type { Password } from "../../../components/templates/Password/types/Password"
 import type { ZodString } from "zod"
 
-export const passwordChangeSchema = z.object({
+export const passwordChangeSchema = {
   confirmationPassword: passwordSchema,
   currentPassword: passwordSchema,
   newPassword: passwordSchema,
-} satisfies Record<keyof Password, ZodString>)
+} satisfies Record<keyof Password, ZodString>
--- src/shared/schemas/registerSchema/index.ts
+++ src/shared/schemas/registerSchema/index.ts
@@ -3,23 +3,26 @@ import { z } from "zod"
 import { passwordSchema } from "../passwordSchema"
 
 import type { Register } from "../../../components/templates/Register/types/Register"
-import type { ZodObject, ZodString } from "zod"
+import type { ZodString } from "zod"
 
 type NameSchemaType = Record<keyof Register["name"], ZodString>
 type BirthdaySchemaType = Record<keyof Register["birthday"], ZodString>
 
-export const registerSchema = z.object({
+export const registerSchema = {
   /* eslint-disable @typescript-eslint/no-magic-numbers */
-  birthday: z.object({
+  birthday: {
     date: z.string().min(1).max(2),
     month: z.string().min(1).max(2),
     year: z.string().min(4).max(4),
-  } satisfies BirthdaySchemaType),
+  } satisfies BirthdaySchemaType,
   id: z.string().min(8, "Password must contain at least 8 characters"),
-  name: z.object({
+  name: {
     firstName: z.string().min(1, "Required"),
     lastName: z.string().min(1, "Required"),
-  } satisfies NameSchemaType),
+  } satisfies NameSchemaType,
   password: passwordSchema,
   /* eslint-enable @typescript-eslint/no-magic-numbers */
-} satisfies Record<keyof Register, ZodObject<BirthdaySchemaType | NameSchemaType> | ZodString>)
+} satisfies Record<
+  keyof Register,
+  BirthdaySchemaType | NameSchemaType | ZodString
+>
