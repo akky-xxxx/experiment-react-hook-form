fcc2f2401283326aa961d85acc77a975f6d1a390
akky-xxxx <114.s.aki@gmail.com>
2023-08-19 21:11:53 +0900

    refactor: specify type to Text, NamesFields, BirthdayFields

--- src/components/templates/Register/View.tsx
+++ src/components/templates/Register/View.tsx
@@ -31,11 +31,11 @@ export const RegisterView: FC<Props> = (props) => {
         <legend>Input ID and Password.</legend>
 
         <div>
-          <Text labelText="ID" name="id" register={register} />
+          <Text<Register> labelText="ID" name="id" register={register} />
         </div>
 
         <div>
-          <Text
+          <Text<Register>
             labelText="Password"
             name="password"
             register={register}
@@ -44,14 +44,23 @@ export const RegisterView: FC<Props> = (props) => {
         </div>
       </fieldset>
 
-      <NameFields fieldMessage="Input your name." register={register} />
+      <NameFields
+        fieldMessage="Input your name."
+        names={{ firstName: "name.firstName", lastName: "name.lastName" }}
+        register={register}
+      />
 
-      <BirthdayFields
+      <BirthdayFields<Register>
         control={control}
         dates={dates}
         fieldMessage="Select your birthday."
         isDateEnable={isDateEnable}
         isMonthEnable={isMonthEnable}
+        names={{
+          date: "birthday.date",
+          month: "birthday.month",
+          year: "birthday.year",
+        }}
       />
 
       <button type="submit">Confirm</button>
