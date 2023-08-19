c2c4e0df31490a0fa88e3e5f2ca0879b4634c837
akky-xxxx <114.s.aki@gmail.com>
2023-08-20 01:06:58 +0900

    feat: extends Test function

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -1,20 +1,29 @@
 import type { InputHTMLAttributes } from "react"
-import type { FieldPath, FieldValues, UseFormRegister } from "react-hook-form"
+import type {
+  FieldPath,
+  FieldValues,
+  UseFormRegister,
+  RegisterOptions,
+} from "react-hook-form"
 
-type Props<F extends FieldValues> = {
-  name: FieldPath<F>
+type Props<
+  F extends FieldValues,
+  N extends FieldPath<F> = FieldPath<F>,
+> = Partial<RegisterOptions<F, N>> & {
+  name: N
   labelText: string
   register: UseFormRegister<F>
   type?: InputHTMLAttributes<"HTMLAttributes">["type"]
+  validate?: RegisterOptions<F>["validate"]
 }
 
 export const Text = <F extends FieldValues>(props: Props<F>) => {
-  const { name, labelText, register, type } = props
+  const { name, labelText, register, type, ...registerOptions } = props
 
   return (
     <label>
       <span>{labelText}：</span>
-      <input {...register(name, { required: true })} type={type || "text"} />
+      <input {...register(name, registerOptions)} type={type || "text"} />
     </label>
   )
 }
