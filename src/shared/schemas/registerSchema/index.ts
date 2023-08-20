6ba7085477d605108839814dac5c66d8cfb73b41
akky-xxxx <114.s.aki@gmail.com>
2023-08-20 19:41:52 +0900

    refactor: adjust registerSchema

--- src/shared/schemas/registerSchema/index.ts
+++ src/shared/schemas/registerSchema/index.ts
@@ -3,17 +3,20 @@ import { z } from "zod"
 import { passwordSchema } from "../passwordSchema"
 
 import type { Register } from "../../../components/templates/Register/types/Register"
-import type { ZodString } from "zod"
+import type { ZodString, ZodEffects } from "zod"
 
 type NameSchemaType = Record<keyof Register["name"], ZodString>
-type BirthdaySchemaType = Record<keyof Register["birthday"], ZodString>
+type BirthdaySchemaType = Record<
+  keyof Register["birthday"],
+  ZodEffects<ZodString, number, string>
+>
 
 export const registerSchema = {
   /* eslint-disable @typescript-eslint/no-magic-numbers */
   birthday: {
-    date: z.string().min(1).max(2),
-    month: z.string().min(1).max(2),
-    year: z.string().min(4).max(4),
+    date: z.string().min(1).max(2).transform(Number),
+    month: z.string().min(1).max(2).transform(Number),
+    year: z.string().min(4).max(4).transform(Number),
   } satisfies BirthdaySchemaType,
   id: z.string().min(8, "Password must contain at least 8 characters"),
   name: {
