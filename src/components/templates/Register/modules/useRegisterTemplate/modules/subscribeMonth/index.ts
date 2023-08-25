989964e2b37ad6089a6e44bb216075fe957b35d0
akky-xxxx <114.s.aki@gmail.com>
2023-08-25 22:28:57 +0900

    feat: optimize subscribeMonth

--- src/components/templates/Register/modules/useRegisterTemplate/modules/subscribeMonth/index.ts
+++ src/components/templates/Register/modules/useRegisterTemplate/modules/subscribeMonth/index.ts
@@ -2,12 +2,8 @@ import type { Subscribe } from "../../../../../../../shared/types/Subscribe"
 import type { Register } from "../../../../types/Register"
 
 export const subscribeMonth: Subscribe<Register> = (subscribeArguments) => {
-  const { formState, name, setValue, type } = subscribeArguments
+  const { name, setValue, type } = subscribeArguments
   if (name === "birthday.month" && type === "change") {
-    const isSelectedBirthdayMonth = Boolean(formState.birthday?.month)
     setValue("birthday.date", "")
-    if (!isSelectedBirthdayMonth) {
-      setValue("birthday.date", "")
-    }
   }
 }
